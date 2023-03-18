5fb192d2bcf5c1e67f96b090400699df1d502109
shubhampoddar01 <shubham.poddar0105@gmail.com>
2023-03-19 00:33:05 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import  {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Addproduct from './components/Addproduct';
+import Cart from './components/Cart';
+import Details from './components/Details';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router>
+      <Routes>
+    <Route exact path ="/" element={<App />} />
+    <Route exact path ="/addproduct" element={<Addproduct/>} />
+    <Route exact path ="/cart" element={<Cart/>}/>
+    <Route exact path='/details/:id' element={<Details data={store}/>}/>
+    </Routes>
+    </Router>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
