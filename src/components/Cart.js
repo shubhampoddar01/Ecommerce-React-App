255fa7902eda8128806e0ae652ace923885e3798
shubhampoddar01 <shubham.poddar0105@gmail.com>
2023-03-25 01:01:57 +0530

    Completing the ecommerce app

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,81 +1,52 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { CartItems } from "../actions";
-import { updateCart, DeleteCart } from "../actions";
+import React, { useState, useEffect } from "react";
+import "../styles/cart.css";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCartItem } from "./../Redux/CartSlice"
+ 
 
-export default function Cart({ item }) {
-  const dispatchPlus = useDispatch();
-  const dispatchMinus = useDispatch();
-  const dispatchTotal = useDispatch();
-  const dispatchDelete = useDispatch();
-  // increase quantity of product
-  function handlePlus(item) {
-    item.qty += 1;
-    dispatchPlus(updateCart(item));
-    dispatchTotal(CartItems());
-  }
-  // decrease quantity of product
-  function handleMinus(item) {
-    if (item.qty > 1) {
-      item.qty -= 1;
-      dispatchMinus(updateCart(item));
-      dispatchTotal(CartItems());
-    }
-  }
-  // delete product from cart
-  function handleCancel(item) {
-    dispatchDelete(DeleteCart(item));
-    dispatchTotal(CartItems());
-  }
+const Cart = () => {
+  const dispatch = useDispatch()
+  const [price, setPrice] = useState(0);
+  const [currentItems, setCurrentItems] = useState();
+  const lengthItems = useSelector((state) => state.cartDetail.value);
+
+  const handleRemove = (e, item) => {
+    dispatch(removeCartItem(item))
+    setCurrentItems(lengthItems);
+  };
+
+  const handlePrice = () => {
+    let ans = 0;
+    lengthItems?.map((item) => ans += item.price);
+    setPrice(ans);
+  };
+
+  useEffect(() => {
+    handlePrice();
+    setCurrentItems(lengthItems);
+
+  }, [lengthItems]);
+
   return (
-    <>
-      {/* items added to Cart  */}
-      <div className="d-flex container-sm p-1 bg-white  gap-5">
-        {/* left part  */}
-        <img
-          src={item.thumbnail}
-          alt="error"
-          id="card-image "
-          style={{ width: "50%", height: "17rem", objectFit: "cover" }}
-        />
-        {/* right-part  */}
-        <div
-          className="d-flex flex-column gap-2 justify-content-center"
-          style={{ width: "50%" }}
-        >
-          <span>{item.title}</span>
-          <span className="text-success">
-            <span className="text-danger">Price:</span>Rs{item.price}
-          </span>
-
-          <div className="d-flex gap-3 mt-4">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/6941/6941676.png"
-              alt="error"
-              width={"30rem"}
-              onClick={() => handlePlus(item)}
-            />
-            <span className=" border border-1 border-dark px-4">
-              {item.qty}
-            </span>
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/1828/1828777.png"
-              alt="error"
-              width={"30rem"}
-              onClick={() => handleMinus(item)}
-            />
+    <article>
+      {currentItems?.map((item) => (
+        <div className="cart_box" key={item.id}>
+          <div className="cart_img">
+            <img src={item.thumbnail} alt="" />
+            <p>{item.title}</p>
           </div>
-          <div className="align-self-end mt-5 ">
-            <button
-              type="button"
-              className="btn btn-outline-secondary"
-              onClick={() => handleCancel(item)}
-            >
-              Cancel
-            </button>
+          <div>
+            <span>{'Price :-' + ' ' + item.price}</span>
+            <button className="btn btn-danger d-flex" onClick={(e) => handleRemove(e, item)}>Remove</button>
           </div>
         </div>
+      ))}
+      <div className="text-dark d-flex justify-content-between">
+        <h3>Total Price of your Cart</h3>
+        <span>Rs - {price}</span>
       </div>
-    </>
+    </article>
   );
-}
+};
+
+export default Cart;
